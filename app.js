f22213df64fcba47413ea5cbd187380920b6f881
happypappyhappy <happypappyhappy@proton.me>
2023-03-08 13:39:29 -0800

    asdf
    asdf

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 // app.js
 import { ethers } from "./ethers.esm.min.js";
 
+const connectWallet = async () => {
+  console.log("connect wallet");
+};
+
+const betOne = async () => {
+  console.log("bet one");
+};
+
+const betTwo = async () => {
+  console.log("bet two");
+};
+
+const forceRoll = async () => {
+  console.log("bet forceRoll");
+};
+
+const setRandomSeed = async () => {
+  console.log("set random seed");
+};
+
 export async function main() {
-  const wallet = await ethers.Wallet.createRandom();
-  console.log(`wallet address: ${await wallet.getAddress()}`);
+  const betOneBtn = document.getElementById("bet1Btn");
+  betOneBtn.onclick = await betOne();
+
+  const betTwoBtn = document.getElementById("bet2Btn");
+  betTwoBtn.onclick = await betTwo();
+
+  const forceRollBtn = document.getElementById("forceRollBtn");
+  forceRollBtn.onclick = await forceRoll();
+
+  const setRandomSeedBtn = document.getElementById("setRandomSeedBtn");
+  setRandomSeedBtn.onclick = await setRandomSeed();
+
+  await connectWallet();
 }
